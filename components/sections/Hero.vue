d40913b0e9708ace98b3a8003a673475bb64c3fb
frontend-samir <frontendsamir@gmail.com>
2024-10-23 09:22:47 +0700

    Revert "commit3"
    This reverts commit c8820f804e35db76ffac38ff6c1bf64e942b6a13.

--- components/sections/Hero.vue
+++ components/sections/Hero.vue
@@ -8,6 +8,7 @@ const store = useGeneralStore()
       <div
         class="flex justify-between font-[Satoshi-Medium] text-lg md:text-xl text-gray-100 cursor-default"
       >
+        <h1>....</h1>
       </div>
     </div>
     <ClientOnly>
